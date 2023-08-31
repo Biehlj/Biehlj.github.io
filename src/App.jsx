28363b8fbc50be6bf372e342b9d82be7902fcd2a
John B <128255094+Biehlj@users.noreply.github.com>
2023-08-31 17:21:48 -0500

    Starting a new Portfolio project (#14)
    * Starting a new Portfolio project
    	modified:   src/App.jsx
    	modified:   src/Components/Home.jsx
    
    * Updating package.json file with the following changes:
        Project name: portfolio-site
        URL: www.biehl-net.com
    
    * Update 2
    
    * update package-lock.json
    
    * Add files via upload
    
    Background image for use on portfolio page
    
    * Major Update to Portfolio
    
    * Formatting and updates to content
    
    * Update package.json
    
    * Fixing styles sheet
    
    * Fixing List Format
    
    Signed-off-by: John Biehl <johnbiehl@gmail.com>
    
    * Content Update
    
    Signed-off-by: John Biehl <johnbiehl@gmail.com>
    
    * prittier fix
    
    Signed-off-by: John Biehl <johnbiehl@gmail.com>
    
    ---------
    
    Signed-off-by: John Biehl <johnbiehl@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-/**
- * Application component
- *
- * To contain application wide settings, routes, state, etc.
- */
-
 import React from "react";
 
 import About from "./Components/About";
@@ -11,28 +5,18 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
+import Education from "./Components/Education";
 
-import "./styles.css";
-
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "John Biehl",
+  title: "Cybersecurity Engineer, Developer, and Lifelong Student",
+  email: "johnbiehl@gmail.com",
+  gitHub: "https://github.com/Biehlj",
+  instagram: "https://www.instagram.com/johnpbiehl/",
+  linkedIn: "https://www.linkedin.com/in/johnbiehl/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/biehl_john",
+  youTube: "https://www.youtube.com/c/JohnBiehl47",
 };
 
 const primaryColor = "#4E567E";
@@ -44,6 +28,7 @@ const App = () => {
       <Header />
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
+      <Education />
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,51 +1,20 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
 import React from "react";
 
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Information Systems student studying at Southwestern Illinois College. I enjoy utilizing the skills I've learned to create products for myself and others.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
 const skillsList = [
+  "Cybersecurity",
+  "Application development",
+  "DevSecOps practices",
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "The best way to predict the future is to create it. - Abraham Lincoln";
 
 const About = () => {
   return (
@@ -56,24 +25,16 @@ const About = () => {
           backgroundColor: "white",
           width: "50%",
           padding: "4rem",
-          margin: "3rem auto",
+          margin: "0 auto",
           textAlign: "center",
         }}
       >
         <h2>About Myself</h2>
         <p className="large">{description}</p>
         <hr />
-        <ul
-          style={{
-            textAlign: "left",
-            columns: 2,
-            fontSize: "1.25rem",
-            margin: "2rem 3rem",
-            gap: "3rem",
-          }}
-        >
-          {skillsList.map((skill) => (
-            <li key={skill}>{skill}</li>
+        <ul className="about">
+          {skillsList.map((skill, index) => (
+            <li key={index}>{skill}</li>
           ))}
         </ul>
         <hr />
